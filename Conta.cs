43ca23a3bc3dd30748644606360211852eceb471
Rosindo Bandeira <rosindoband@hotmail.com>
2018-10-23 09:38:41 +0100

    commit before starts remote

--- Conta.cs
+++ Conta.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace fix1
+﻿namespace fix1
 {
     class Conta
     {
